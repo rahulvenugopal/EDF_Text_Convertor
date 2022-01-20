11fb66d586707cc2b910a8743fc57bf2871f1df9
rahulvenugopal <rhlvenugopal@gmail.com>
2022-01-20 12:51:52 +0530

    Batch conversion added
    .m and .py scripts work in batch mode now

--- EDF_text_relabel_txt.py
+++ EDF_text_relabel_txt.py
@@ -5,50 +5,58 @@ Created on Wed Jan 19 13:54:44 2022
 - Get the text file spit out by MATLAB
 - Remove columns and reatian only the sleep stages one
 - Replace the string by numbers
-
+- Looping for batch processing multiple text files
 @author: Rahul Venugopal
 """
 
 #%% Load the text file and start spliting
 
-# Load data
-f = open('BHBCFH_SleepStageList.txt',"r")
-lines = f.readlines()
+# Get all text files
+from glob import glob
+filelist = glob('*.txt')
 
-result=[]
+for files in filelist:
+    # Load data
+    f = open(files,"r")
+    lines = f.readlines()
 
-for rows in lines:
-    result.append(rows.split(' ')[2])
-f.close()
+    result=[]
 
-stage_list = []
-for slices in result:
-    stage_list.append(slices.split('\t')[0])
+    for rows in lines:
+        result.append(rows.split(' ')[2])
+    f.close()
 
-# string repalce
-stage_list = list(map(lambda stage_list: str.replace(stage_list, "W", "0"), stage_list))
+    stage_list = []
+    for slices in result:
+        stage_list.append(slices.split('\t')[0])
 
-stage_list = list(map(lambda stage_list: str.replace(stage_list, "N1", "1"), stage_list))
+    # string repalce
+    stage_list = list(map(lambda stage_list: str.replace(stage_list, "W", "0"),
+                          stage_list))
 
-
-stage_list = list(map(lambda stage_list: str.replace(stage_list, "N2", "2"), stage_list))
-
-
-stage_list = list(map(lambda stage_list: str.replace(stage_list, "N3", "3"), stage_list))
-
-
-stage_list = list(map(lambda stage_list: str.replace(stage_list, "R", "5"), stage_list))
-
-# writing varibale to a text file
-# open file
-with open('sleep_stage_list.txt', 'w+') as f:
-
-    # write elements of list
-    for items in stage_list:
-        f.write('%s\n' %items)
-
-    print("File written successfully")
+    stage_list = list(map(lambda stage_list: str.replace(stage_list, "N1", "1"),
+                          stage_list))
 
 
-# close the file
-f.close()
+    stage_list = list(map(lambda stage_list: str.replace(stage_list, "N2", "2"),
+                          stage_list))
+
+
+    stage_list = list(map(lambda stage_list: str.replace(stage_list, "N3", "3"),
+                          stage_list))
+
+
+    stage_list = list(map(lambda stage_list: str.replace(stage_list, "R", "5"), stage_list))
+
+    # writing varibale to a text file
+    # open file
+    with open(files, 'w+') as f:
+
+        # write elements of list
+        for items in stage_list:
+            f.write('%s\n' %items)
+
+        print("File %s written successfully" %files)
+
+    # close the file
+    f.close()
--- EDF_to_text.m
+++ EDF_to_text.m
@@ -2,16 +2,24 @@
 % Convert EDF scored file to a text file
 % It uses two functions - NPhy_Sleep_StageListCreator and ReadEDF
 % Below segment of code adds these two functions to path
+% Added a loop to batch convert EDF files to text on 20th Jan 2022 by RVG
 
+% Adding functions to path
 selpath = uigetdir({},'Select the folder which has relevant functions')
 addpath(selpath)
 
 % select file
-[file_selected,file_path] = uigetfile({'*.*',  'All Files (*.*)'},'Select the EDF file to be converted to text')
-[filepath,filename,extension] = fileparts(file_selected)
+[file_selected,file_path] = uigetfile({'*.edf*',  'All EDF files (*.*)'},'Select the EDF files to be converted to text',...
+    'MultiSelect','on')
 
 % changing working directory to where edf file lies
 cd (file_path)
 
-% Run the EDF to text file conversion
-NPhy_Sleep_StageListCreator(filename);
+% get file names
+[filepath,filename,extension] = fileparts(file_selected);
+
+for files = 1:length(filename)
+    % Run the EDF to text file conversion
+    NPhy_Sleep_StageListCreator(filename{files});
+    sprintf('%s.edf converted successfully\n', filename{files})
+end
